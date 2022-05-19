fe89f34fa5c214d2ec519576dd548339cf107073
thepicture <85333076+thepicture@users.noreply.github.com>
2022-05-19 19:28:49 +0500

    refactor(Comments): move the templates to the separate namespace

--- WorldFestSolution.XamarinApp/WorldFestSolution.XamarinApp/Views/Templates/NoItemsTemplate.xaml.cs
+++ WorldFestSolution.XamarinApp/WorldFestSolution.XamarinApp/Views/Templates/NoItemsTemplate.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace WorldFestSolution.XamarinApp.Views.Templates
@@ -12,6 +6,21 @@ namespace WorldFestSolution.XamarinApp.Views.Templates
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NoItemsTemplate : ContentView
     {
+
+
+        public string Reason
+        {
+            get { return (string)GetValue(ReasonProperty); }
+            set { SetValue(ReasonProperty, value); }
+        }
+
+        public static readonly BindableProperty ReasonProperty =
+            BindableProperty.Create("Reason",
+                                    typeof(string),
+                                    typeof(NoItemsTemplate),
+                                    default(string));
+
+
         public NoItemsTemplate()
         {
             InitializeComponent();
