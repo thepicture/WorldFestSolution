cd48af9fb22fcee90308ed38a9ff36e84cfc4d1b
thepicture <85333076+thepicture@users.noreply.github.com>
2022-05-20 16:23:01 +0500

    refactor(Participants): move the data templates to the separate namespace

---
+++ WorldFestSolution.XamarinApp/WorldFestSolution.XamarinApp/Views/Templates/ParticipantsTemplate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WorldFestSolution.XamarinApp.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ParticipantsTemplate : ContentView
+    {
+        public ParticipantsTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
