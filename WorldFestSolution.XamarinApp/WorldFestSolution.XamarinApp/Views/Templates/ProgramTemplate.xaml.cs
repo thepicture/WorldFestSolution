30202f2cf3b1ddfb3214b8e8410c8c73e10b8b98
thepicture <85333076+thepicture@users.noreply.github.com>
2022-05-27 15:06:50 +0500

    refactor(AddEditFestival): move the templates to their respective namespace

---
+++ WorldFestSolution.XamarinApp/WorldFestSolution.XamarinApp/Views/Templates/ProgramTemplate.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WorldFestSolution.XamarinApp.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgramTemplate : ContentView
+    {
+        public ProgramTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
